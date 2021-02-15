364cb65cee6d84bb8c7a4b407a63e65673c1d3a2
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-14 19:50:48 -0600

    Update CreateSupplierPartTable.sql

--- CreateScripts/CreateSupplierPartTable.sql
+++ CreateScripts/CreateSupplierPartTable.sql
@@ -2,6 +2,6 @@ CREATE TABLE Supplier_Part (
 
 SUP_PART_NUMBER varchar(15) PRIMARY KEY not null,
 SUP_PART_DATE DATE not null, /*NEEDS TO BE REVIEWED*/
-SUP_PART_PRICE int not null,
+SUP_PART_PRICE float not null,
 
 );
