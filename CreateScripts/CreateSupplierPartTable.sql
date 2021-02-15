c1adf17eaf3e9abfdc7aa94ceb20938b8cde3bd5
zachkurtubi <zachary.t.a.k@hotmail.com>
2021-02-14 19:51:45 -0600

    Update CreateSupplierPartTable.sql

--- CreateScripts/CreateSupplierPartTable.sql
+++ CreateScripts/CreateSupplierPartTable.sql
@@ -4,4 +4,8 @@ SUP_PART_NUMBER varchar(15) PRIMARY KEY not null,
 SUP_PART_DATE DATE not null, /*NEEDS TO BE REVIEWED*/
 SUP_PART_PRICE float not null,
 
+  
+PRIMARY KEY (SUP_PART_ID),
+FOREIGN KEY (SUP_PART_ID) REFERENCES (Supplier_Lookup)
+
 );
