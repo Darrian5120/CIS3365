d12c5bca46cf3fe8280a55ec531bf8eb15f666b9
zachkurtubi <zachary.t.a.k@hotmail.com>
2021-02-14 19:59:02 -0600

    Update CreateStateTable.sql

--- CreateScripts/CreateStateTable.sql
+++ CreateScripts/CreateStateTable.sql
@@ -2,5 +2,9 @@ CREATE TABLE State(
 
 C_STATE varchar(15) PRIMARY KEY not null,
 LAW_CODE varchar(15) not null,
+  
+  /* FOREIGN KEY (C_STATE) REFERENCES Customer_Contact_Info(C_STATE) *?
+/*Zach Kurtubi*/
+
 
 )
