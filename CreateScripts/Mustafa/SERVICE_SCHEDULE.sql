3e965c0144e500ac880f2806c4a1b35e6c42d7af
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-22 22:49:14 -0600

    Update SERVICE_SCHEDULE.sql

--- CreateScripts/Mustafa/SERVICE_SCHEDULE.sql
+++ CreateScripts/Mustafa/SERVICE_SCHEDULE.sql
@@ -1,7 +1,7 @@
-CREATE TABLE SERVICE_SCHEDULE (
-SCHEDULE_ID int not null,
-SERVICE_REQUESTED varchar(30) not null,
-SERVICE_DATE datetime not null
-PRIMARY KEY (SCHEDULE_ID),
-
-)
+USE CoogTechSolutions
+CREATE TABLE dbo.SERVICE_SCHEDULE (
+	SCHEDULE_ID int not null,
+	SERVICE_TYPE varchar(30) not null,
+	SERVICE_DATE datetime not null
+	PRIMARY KEY (SCHEDULE_ID),
+);
