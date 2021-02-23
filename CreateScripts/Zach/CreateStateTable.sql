266be0aebb91a4a099cd81e9d3c69cc93036fb45
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-22 21:57:35 -0600

    Update CreateStateTable.sql

--- CreateScripts/Zach/CreateStateTable.sql
+++ CreateScripts/Zach/CreateStateTable.sql
@@ -1,10 +1,10 @@
-CREATE TABLE State(
-
-C_STATE varchar(15) PRIMARY KEY not null,
-LAW_CODE varchar(15) not null,
-  
- FOREIGN KEY (C_STATE) REFERENCES CUSTOMER_CONTACT_INFO(C_STATE),
- FOREIGN KEY (C_ID) REFERENCES CUSTOMER_CONTACT_INFO(C_ID)
+USE CoogTechSolutions
+CREATE TABLE dbo.State(
+	C_STATE varchar(15) not null,
+	C_ID int not null,
+	LAW_CODE varchar(15) not null,  
+	PRIMARY KEY(C_STATE, LAW_CODE),
+	FOREIGN KEY (C_ID, C_STATE) REFERENCES CUSTOMER_CONTACT_INFO(C_ID, C_STATE)
  
-/*Zach Kurtubi*/
+	/*Zach Kurtubi*/
 );
