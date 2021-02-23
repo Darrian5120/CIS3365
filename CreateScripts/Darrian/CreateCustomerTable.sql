47e2410993e4838d0c700a613638d21763d44ed3
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-22 21:02:02 -0600

    Update CreateCustomerTable.sql

--- CreateScripts/Darrian/CreateCustomerTable.sql
+++ CreateScripts/Darrian/CreateCustomerTable.sql
@@ -2,7 +2,7 @@ USE CoogTechSolutions
 CREATE TABLE dbo.Customer (
     C_ID int IDENTITY(1,1) PRIMARY KEY,
     C_LNAME varchar(15) NOT NULL,
-    C_FNAME varchar(15) NOT NULL,
+    C_FNAME varchar(15),
     SERVICE_ID int,
     ORDER_ID int,
     C_BUSINESS_NAME varchar(50)
