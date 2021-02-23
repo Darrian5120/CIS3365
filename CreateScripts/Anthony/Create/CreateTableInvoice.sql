1327e36f45c779ec7ffe0d8dbbfe7ad19d677dc7
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-22 22:19:59 -0600

    Update CreateTableInvoice.sql

--- CreateScripts/Anthony/Create/CreateTableInvoice.sql
+++ CreateScripts/Anthony/Create/CreateTableInvoice.sql
@@ -2,12 +2,9 @@ USE CoogTechSolutions
 CREATE TABLE dbo.INVOICE (
 	INVOICE_ID int not null,
 	C_ID int not null,
-	ITEM_ID int not null,
 	AMT_PAID float,
 	AMT_OWED float,
-	ITEM_QUANTITY int,
-	ITEM_COST float,
-	I_DATE  datetime not null,
+	INVOICE_DATE  datetime not null,
 	PRIMARY KEY (INVOICE_ID),
 	FOREIGN KEY (C_ID) REFERENCES Payment(C_ID)
 );
