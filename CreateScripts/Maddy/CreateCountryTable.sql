189b1627b9ae67055bb18838a377b3ae0aca822f
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-22 22:20:03 -0600

    Update CreateCountryTable.sql

--- CreateScripts/Maddy/CreateCountryTable.sql
+++ CreateScripts/Maddy/CreateCountryTable.sql
@@ -1,6 +1,7 @@
 USE CoogTechSolutions
-CREATE TABLE COUNTRY (
+CREATE TABLE dbo.COUNTRY (
 	C_COUNTRY varchar(20) not null,
 	C_ID int not null,
-	PRIMARY KEY (C_COUNTRY)
+	PRIMARY KEY (C_COUNTRY),
+	FOREIGN KEY (C_ID) REFERENCES Customer(C_ID)
 );
