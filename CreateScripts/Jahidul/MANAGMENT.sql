15cda4d53af10daa0dd96091876e8d9a1090565b
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-22 23:32:27 -0600

    Update MANAGMENT.sql

--- CreateScripts/Jahidul/MANAGMENT.sql
+++ CreateScripts/Jahidul/MANAGMENT.sql
@@ -1,16 +1,7 @@
-USE [Coog_Tech_Solutions]
-GO
-
-/****** Object:  Table [dbo].[MANAGMENT]    Script Date: 2/21/2021 8:28:59 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[MANAGMENT](
-	[EMP_ID] [int] NULL
-) ON [PRIMARY]
-GO
+USE CoogTechSolutions
+CREATE TABLE dbo.MANAGMENT(
+	EMP_ID int PRIMARY KEY,
+	FOREIGN KEY (EMP_ID) REFERENCES EMPLOYEE(EMP_ID)
+);
 
 
