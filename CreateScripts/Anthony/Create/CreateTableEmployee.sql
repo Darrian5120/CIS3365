8689eca7d2de80614738404fcb9881e61f509b08
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-22 23:32:03 -0600

    Update CreateTableEmployee.sql

--- CreateScripts/Anthony/Create/CreateTableEmployee.sql
+++ CreateScripts/Anthony/Create/CreateTableEmployee.sql
@@ -1,14 +1,14 @@
-CREATE TABLE EMPLOYEE (
-	EMP_ID int not null,
+USE CoogTechSolutions
+CREATE TABLE dbo.EMPLOYEE (
+	EMP_ID int IDENTITY(1,1) PRIMARY KEY,
 	EMP_LNAME varchar(30) not null,
 	EMP_FNAME varchar(30) not null,
 	EMP_ADDRESS varchar(50) not null,
 	EMP_HIRE_DATE DATE,
-	EMP_HOURS varchar(4),
+	EMP_HOURS float,
 	EMP_PAY_RATE float,
 	EMP_PHONE int not null,
 	EMP_BANK_INFO int,
 	EMP_TAX int,
-	EMP_JOB_FUNC varchar(30),
-	PRIMARY KEY (EMP_ID)
+	EMP_JOB_FUNC varchar(30)
 );
