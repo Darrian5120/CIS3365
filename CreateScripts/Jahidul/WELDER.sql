51529ab3cdb0e34077cb79f3cd5f5cb6fb597b85
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-22 23:32:32 -0600

    Update WELDER.sql

--- CreateScripts/Jahidul/WELDER.sql
+++ CreateScripts/Jahidul/WELDER.sql
@@ -1,16 +1,5 @@
-USE [Coog_Tech_Solutions]
-GO
-
-/****** Object:  Table [dbo].[WELDER]    Script Date: 2/21/2021 8:29:43 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[WELDER](
-	[EMP_ID] [int] NULL
-) ON [PRIMARY]
-GO
-
-
+USE CoogTechSolutions
+CREATE TABLE dbo.WELDER(
+	EMP_ID int PRIMARY KEY,
+	FOREIGN KEY (EMP_ID) REFERENCES EMPLOYEE(EMP_ID)
+);
