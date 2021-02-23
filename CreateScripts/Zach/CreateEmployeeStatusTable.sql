1f3a8a8fba4252a1800f67570986d5f9c74f694a
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-02-22 23:32:44 -0600

    Update CreateEmployeeStatusTable.sql

--- CreateScripts/Zach/CreateEmployeeStatusTable.sql
+++ CreateScripts/Zach/CreateEmployeeStatusTable.sql
@@ -1,10 +1,9 @@
-CREATE TABLE Employee_Status(
-
-	EMP_ID int,
+USE CoogTechSolutions
+CREATE TABLE dbo.Employee_Status(
+	EMP_ID int not null,
 	EMP_ACTIVE BIT,
-
 	PRIMARY KEY(EMP_ID),
-  FOREIGN KEY (EMP_ID) REFERENCES Employee(EMP_ID)
+	FOREIGN KEY (EMP_ID) REFERENCES Employee(EMP_ID)
   
 /*Zach Kurtubi*/
 
