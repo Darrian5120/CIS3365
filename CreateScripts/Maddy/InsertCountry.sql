a15ff550405668696916f8b73f0e05b5ed5caaaf
madalynpeden <78273237+madalynpeden@users.noreply.github.com>
2021-02-24 12:03:45 -0600

    Update InsertCountry.sql

--- CreateScripts/Maddy/InsertCountry.sql
+++ CreateScripts/Maddy/InsertCountry.sql
@@ -1,4 +1,4 @@
 “SET IDENTITY_INSERT CoogTechSolutions.dbo.COUNTRY OFF; 
-INSERT INTO CoogTechSolutions.dbo.COUNTRY (C_COUNTRY, C_ID) 
-VALUES (?,?)
-“,row.C_COUNTRY,row.C_ID)
+INSERT INTO CoogTechSolutions.dbo.COUNTRY (C_ID) 
+VALUES (?)
+“,row.C_ID)
