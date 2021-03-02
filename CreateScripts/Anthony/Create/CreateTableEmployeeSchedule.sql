1323f2a6c4fbae43336c83a6f89ca10054ff8a44
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-03-02 15:38:56 -0600

    Delete CreateTableEmployeeSchedule.sql

--- CreateScripts/Anthony/Create/CreateTableEmployeeSchedule.sql
+++
@@ -1,7 +0,0 @@
-USE CoogTechSolutions
-CREATE TABLE dbo.EMPLOYEE_SCHEDULE (
-	SCHEDULE_ID int not null,
-	EMP_SCHD_WEEK DATE not null,
-	EMP_SCHD_EMP_IDS int not null,
-	PRIMARY KEY (SCHEDULE_ID)
-);
