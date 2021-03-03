381a091278a6252ae914ccd9b168f245c1b3cf13
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-03-03 13:30:19 -0600

    fix anthony

--- alter_scripts.sql
+++ alter_scripts.sql
@@ -1,9 +1,12 @@
+USE CoogTechSolutions
+-------------------------Darrian-----------------------------------------------------
 --Alter Payment--
 ALTER TABLE PAYMENT
-add CONSTRAINT C_ID_FK FOREIGN KEY (C_ID) REFERENCES Customer(C_ID);
+add CONSTRAINT CUSTOMER_ID_FK FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID);
 --Alter Service Order Line--
 ALTER TABLE SERVICE_ORDER_LINE
 add CONSTRAINT ORDER_ID_FK FOREIGN KEY (ORDER_ID) REFERENCES FINSIHED_ORDER(ORDER_ID);
 --Alter Finsihed Order--
 ALTER TABLE FINSIHED_ORDER
 add CONSTRAINT ORDER_ID_FK FOREIGN KEY (ORDER_ID) REFERENCES SERVICE_REQUEST(ORDER_ID);
+---------------------------------------------------------------------------------------
