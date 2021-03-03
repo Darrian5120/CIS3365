3f4338629929b65243b778b36fd7006337a940d4
Mustafa-alkishtainy <61714430+Mustafa-alkishtainy@users.noreply.github.com>
2021-03-03 13:32:03 -0600

    Create create_scripts.sql

--- create_scripts.sql
+++ create_scripts.sql
@@ -2,17 +2,17 @@
 USE CoogTechSolutions
 -------------------Darrian-------------------------------
 CREATE TABLE dbo.Customer (
-    CUSTOMER_ID int IDENTITY(1,1) NOT NULL,
+    C_ID int IDENTITY(1,1) NOT NULL,
     C_LNAME varchar(15) NOT NULL,
     C_FNAME varchar(15),
     ORDER_ID int,
     C_BUSINESS_NAME varchar(50),
-	CONSTRAINT CUSTOMER_ID_PK PRIMARY KEY (C_ID)
+	CONSTRAINT C_ID_PK PRIMARY KEY (C_ID)
 );
 CREATE TABLE dbo.PAYMENT (
-    CUSTOMER_ID int NOT NULL,
+    C_ID int NOT NULL,
     AMT_PAID float NOT NULL,
-	CONSTRAINT CUSTOMER_ID_PK PRIMARY KEY (C_ID)
+	CONSTRAINT C_ID_PK PRIMARY KEY (C_ID)
 );
 CREATE TABLE dbo.SERVICE_ORDER_LINE (
     ORDER_ID INT NOT NULL,
@@ -81,4 +81,51 @@ CREATE TABLE dbo.INVOICE (
 	AMT_OWED float,
 	INVOICE_DATE  datetime not null,
 	CONSTRAINT INVOICE_ID_PK PRIMARY KEY (INVOICE_ID),
+);
+------------------Mustafa-----------------------------------------------
+--Business info--
+CREATE TABLE dbo.BUSINESS_INFO (
+	BUS_ID int not null,
+	BUS_ADDRESS  varchar(30) not null,
+	BUS_CITY varchar(30) not null,
+	BUS_STATE varchar(2) not null,
+	BUS_ZIP int not null,
+	BUS_PHONE int not null,
+	BUS_FAX int not null,
+	BUS_HOURS int not null,
+	BUS_EMAIL varchar(30) not null,
+	BUS_WEBSITE varchar(30) not null,
+	/*BUS_IMAGES int*/
+	PRIMARY KEY(BUS_ID),
+);
+--current service--
+CREATE TABLE dbo.CURRENT_SERVICE (
+	SERVICE_ID int not null,
+	SERVICE_TYPE varchar(30) not null,
+	V_VIN int not null,
+	C_ID int not null,
+	REQUEST_DATE date not null, 
+	PRIMARY KEY (SERVICE_ID),
+	/*FOREIGN KEY (SERVICE_ID) REFERENCES SERVICE_ORDER_LINE(SERVICE_ID),
+	FOREIGN KEY (V_VIN) REFERENCES VEHICLE(V_VIN),
+	FOREIGN KEY (C_ID) REFERENCES Customer(C_ID)*/
+);
+--customer contact--
+CREATE TABLE dbo.CUSTOMER_CONTACT_INFO (
+	C_ID INT not null,
+	C_PHONE INT  not null,
+	C_EMAIL varchar(30) not null,
+	C_ADDRESS varchar(90) not null,
+	C_ZIP INT not null,
+	C_CITY varchar(30) not null,
+	C_STATE varchar(15) not null,
+	PRIMARY KEY(C_ID, C_STATE), 
+	/*FOREIGN KEY (C_ID) REFERENCES Customer(C_ID)*/
+);
+--service schedule--
+CREATE TABLE dbo.SERVICE_SCHEDULE (
+	SCHEDULE_ID int not null,
+	SERVICE_TYPE varchar(30) not null,
+	SERVICE_DATE datetime not null
+	PRIMARY KEY (SCHEDULE_ID),
 );
