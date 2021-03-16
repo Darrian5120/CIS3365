b091be9ecd5eb816f4031a21cf7a25330825bfa9
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-03-15 21:09:47 -0500

    double check everyone

--- Data/Zach/Insert Service Area.sql
+++
@@ -1,4 +0,0 @@
-''' SET IDENTITY_INSERT CoogTechSolutions.dbo.Service_Area_Part OFF;
- INSERT INTO CoogTechSolutions.dbo.Service_Area (AREA_ID, SERVICE_TYPE, V_ID, INVENTORY)
-VALUES (?,?,?,?) ''', 
-row.AREA_ID, row.SERVICE_Type, row.V_ID, row.INVENTORY)
