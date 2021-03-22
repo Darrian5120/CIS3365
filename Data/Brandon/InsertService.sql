d7b7c02485c3fd3fa1503e4552920481d9b07317
bscrisp <62622283+bscrisp@users.noreply.github.com>
2021-03-22 17:16:57 -0500

    Update InsertService.sql

--- Data/Brandon/InsertService.sql
+++ Data/Brandon/InsertService.sql
@@ -1,4 +1,8 @@
-“SET IDENTITY_INSERT CoogTechSolutions.dbo.Service OFF;  
-INSERT INTO CoogTechSolutions.dbo.Service (SERVICE_TYPE,V_ID,DATE_IN,DATE_OUT,SERVICE_RATE,MATERIALS_REQUIRED) 
-VALUES (?,?,?,?,?,?) 
-“,row.SERVICE_TYPE,row.V_ID,row.DATE_IN,row.DATE_OUT,row.SERVICE_RATE,row.MATERIALS_REQUIRED) 
+Bulk INSERT Service
+	FROM ''
+	WITH (
+			FIRSTROW =2,
+				FIELDTERMINATOR = '|',
+				ROWTERMINATOR = '\n',
+				MAXERRORS = 1
+		);
