31025d51df4bdb08b210b8c74452042921751898
madalynpeden <78273237+madalynpeden@users.noreply.github.com>
2021-03-24 15:03:24 -0500

    Update repairs_report.sql

--- Reports/Maddy/repairs_report.sql
+++ Reports/Maddy/repairs_report.sql
@@ -2,8 +2,10 @@ USE CoogTechSolutions
 
 DECLARE 
 @serviceRepairs varchar(7)
+@trailerRepairs varchar(15)
 
 SELECT Service.SERVICE_ID, Service.SERVICE_NAME
 SELECT @serviceRepairs = 'Repairs'
 
 WHERE Service.SERVICE_NAME like @serviceRepairs
+AND Service.SERVICE_NAME like @trailerRepairs
