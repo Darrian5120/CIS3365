a880d149c65150e67a51a08675b2567130508eb5
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-03-24 16:02:49 -0500

    d

--- Data/Jerry/Create/CreateTableInsurance_Company.sql
+++
@@ -1,8 +0,0 @@
-USE CoogTechSolutions
-CREATE TABLE dbo.INSURANCE_COMPANY (
-	INSURANCE_ID int not null,
-	C_ID int not null,
-	COMPANY_NAME varchar(30) not null,
-	PRIMARY KEY (INSURANCE_ID),
-	FOREIGN KEY (C_ID) REFERENCES Customer(C_ID)
-);
