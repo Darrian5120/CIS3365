91b6dcc9cd979487099b1d2ec0f04dad012ae138
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2021-03-30 20:15:08 -0500

    remove junk

--- Data/Brandon/InsertService.sql
+++
@@ -1,8 +0,0 @@
-Bulk INSERT Service
-	FROM ''
-	WITH (
-			FIRSTROW =2,
-				FIELDTERMINATOR = '|',
-				ROWTERMINATOR = '\n',
-				MAXERRORS = 1
-		);
--- Data/Mustafa/SERVICE_SCHEDULE.sql
+++
@@ -1,7 +0,0 @@
-USE CoogTechSolutions
-CREATE TABLE dbo.SERVICE_SCHEDULE (
-	SCHEDULE_ID int not null,
-	SERVICE_TYPE varchar(30) not null,
-	SERVICE_DATE datetime not null
-	PRIMARY KEY (SCHEDULE_ID),
-);
