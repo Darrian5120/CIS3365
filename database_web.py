d44603a5ed99e51fd26530e13d37a466f4b1d60d
atvu25 <61714399+atvu25@users.noreply.github.com>
2021-04-06 13:11:38 -0500

    Update database_web.py

--- database_web.py
+++ database_web.py
@@ -227,6 +227,40 @@ def inactive_report():
     conn.commit()
     return render_template('report_inactivecustomer.html', data = data)
 
+# Report Long term loyal customer
+# Not sure if path is correct
+@app.route ('/customer/longtermloyalcustomer-report' , methods = ['GET'])
+def longtermloyalcustomer_report():
+    cursor.execute("""
+    SELECT 
+    Customer.CUSTOMER_ID AS "ID",
+    Customer.C_FNAME AS "First Name",
+    Customer.C_LNAME AS "Last Name",
+    CUSTOMER_STATUS.ACTIVE AS "Customer Type",
+    CUSTOMER_VEHICLE.V_VIN AS "VIN #",
+    VEHICLE.V_MAKE AS "Make",
+    VEHICLE.V_MODEL AS "Model",
+    SERVICE_ORDER.ORDER_DATE AS "First Time Customer Date"
+
+    FROM Customer 
+    JOIN CUSTOMER_STATUS
+    ON Customer.CUSTOMER_ID = CUSTOMER_STATUS.CUSTOMER_ID
+    JOIN CUSTOMER_VEHICLE
+    ON Customer.CUSTOMER_ID = CUSTOMER_VEHICLE.CUSTOMER_ID
+    JOIN VEHICLE
+    ON CUSTOMER_VEHICLE.V_VIN = VEHICLE.V_VIN
+    JOIN CUSTOMER_ORDER
+    ON Customer.CUSTOMER_ID = CUSTOMER_ORDER.CUSTOMER_ID
+    JOIN SERVICE_ORDER
+    ON CUSTOMER_ORDER.SERVICE_ORDER_ID = SERVICE_ORDER.SERVICE_ORDER_ID
+
+    WHERE (CUSTOMER_STATUS.C_ACTIVE= 1 OR CUSTOMER_STATUS.C_ACTIVE= 3) AND SERVICE_ORDER.ORDER_DATE < DATEADD(YEAR, -1, GETDATE());
+    """)
+    
+    data = cursor.fetchall()
+    conn.commit()
+    return render_template('report_longtermloyalcustomer.html', data = data)
+
 ################################### VEHICLES ##################################################
 @app.route('/vehicles', methods = ['GET']) 
 def vehicles():
