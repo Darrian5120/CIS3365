563c11e387eefea1748d3977c482d6b5662c056e
Giancarlos Jaen <mmmgian@icloud.com>
2021-04-19 13:56:18 -0500

    finalized reports and rename files

--- Reports/Gian/unavailable_painter_report.sql
+++ Reports/Gian/unavailable_painter_report.sql
@@ -1,14 +1,15 @@
 SELECT EMPLOYEE.EMPLOYEE_LNAME AS 'Last Name',
        EMPLOYEE.EMPLOYEE_FNAME AS 'First Name',
        ROLE.ROLE_NAME AS 'Job Role',
-       ROLE.PAY_RATE AS 'Hourly Pay',
        Employee_Status.ACTIVE_NAME AS 'Status'
 
 FROM EMPLOYEE
-
 JOIN ROLE ON EMPLOYEE.ROLE_ID=ROLE.ROLE_ID
 INNER JOIN Employee_Status ON EMPLOYEE.ACTIVE_ID=Employee_Status.ACTIVE_ID
 
-WHERE Employee_Status.ACTIVE_ID = 1 OR Employee_Status.ACTIVE_ID = 2
 
-ORDER BY [Job Role],[Hourly Pay];
+
+WHERE EMPLOYEE.ROLE_ID = 3
+AND  Employee_Status.ACTIVE_ID = 1
+
+ORDER BY EMPLOYEE_LNAME;
